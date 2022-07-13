c385b5fd4883ef4ab96aee98ca82b5add3d32007
SONY <SONY@DESKTOP-VPEVLE4>
2022-07-13 13:36:51 +0530

    First commit

---
+++ githubdemo/src/main/java/com/pages/LoginPage.java
@@ -0,0 +1,7 @@
+package com.pages;
+
+public class LoginPage {
+ public void loginpage() {
+	 System.out.println("This is login page");
+ }
+}
---
+++ githubdemo/src/test/java/com/testcases/LoginTest.java
@@ -0,0 +1,8 @@
+package com.testcases;
+
+public class LoginTest {
+	public void logintest() {
+		System.out.println("this is login test");
+	}
+
+}
